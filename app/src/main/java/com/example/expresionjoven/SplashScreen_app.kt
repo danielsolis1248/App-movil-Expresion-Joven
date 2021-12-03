41cacd46b60c5f0bf37d02e878a6a8e176425020
jcvisk <juan.carlos.gcn@gmail.com>
2021-12-03 01:25:12 -0600

    corrigiendo splashScreen

--- app/src/main/java/com/example/expresionjoven/SplashScreen_app.kt
+++ app/src/main/java/com/example/expresionjoven/SplashScreen_app.kt
@@ -7,7 +7,7 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen_app : AppCompatActivity() {
-    companion object {
+    companion object{
         const val TIME_SPLASH_SCREEN = 1000L
     }
 
@@ -16,15 +16,15 @@ class SplashScreen_app : AppCompatActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.splashscreen_design)
 
+        exitSplasScreen()
     }
 
-    private fun salirSplashScreen () {
-    Handler(Looper.getMainLooper()).postDelayed({
-        val intent = Intent(this,MainActivity::class.java)
-        startActivity(intent)
-        overridePendingTransition(android.R.anim.bounce_interpolator,android.R.anim.cycle_interpolator)
-        this.finish()}, TIME_SPLASH_SCREEN)
-
+    private fun exitSplasScreen() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            this.finish()
+        }, TIME_SPLASH_SCREEN)
     }
 
 }
